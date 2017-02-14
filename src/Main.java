190c157ead78432b6885cc02c57e79a13c6246a7
negruson <adrinegre3@gmail.com>
2017-02-14 19:56:58 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I am Negre from Negre_Branch and push negreBranch !!!!");
+        System.out.println("Hello World! I am Negre !!!!");
     }
 }
