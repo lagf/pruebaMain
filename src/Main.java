20959e4b7fbca78a6292f52ed509bc75e756b594
negruson <adrinegre3@gmail.com>
2017-02-14 19:29:20 +0100

    cambios en Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I am Negre!!!!");
+        System.out.println("Hello World! I am Negre 2!!!!");
     }
 }
